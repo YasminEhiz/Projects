46eb7be78c134ec1cd7867921519f69b1d4b64db
YasminEhiz <ehizogieyasmin@gmail.com>
2024-07-18 17:40:38 +0100

    .

--- Calculator.myfirstproject/myScript.js
+++ Calculator.myfirstproject/myScript.js
@@ -1,20 +1,58 @@
-const display = document.getElementById("display");
+let firstNumber
+let secondNumber
+let step = 0
+let operation 
+let result = 0
 
-function appendToDisplay(input){
-    display.value += input;
+const numArray = []
+const secondNumArray = []
 
+const display=document.getElementById("display")
+
+function getNumber(num){
+  if(step === 0 || step === 1){
+    numArray.push(num)
+    step = 1
+    firstNumber = Number(numArray.join(''))
+    display.value = firstNumber
+  }else if(step === 2){
+    secondNumArray.push(num)
+    secondNumber = Number(secondNumArray.join(''))
+    display.value = secondNumber
+  }
 }
+
+function getOperator(op){
+  step = 2
+  operation = op
+}
+
 function clearDisplay(){
-    display.value ="";
+  display.value = 0
+  firstNumber = null
+  secondNumber = null
+  step = 0
+  operation = null
+  result = 0 
+  numArray = []
+  secondNumArray = []
 }
-function calculate(){
-    try{
-        display.value = eval(display.value)
-    }
-    catch(error){
-        display.value = "Error"
 
-    }
-   
-
+const calculateEquals = () => {
+  if (operation === '+'){
+    result = firstNumber + secondNumber
+    display.value = result
+  }else if (operation === '-'){
+    result = firstNumber - secondNumber
+    display.value = result
+  }else if (operation === '*'){
+    result = firstNumber * secondNumber
+    display.value = result
+  }else if (operation === '/'){
+    result = firstNumber / secondNumber
+    display.value = result
+  }else if(operation === '%'){
+    result = firstNumber % secondNumber
+    display.value = result
+  }
 }
